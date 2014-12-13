e0364dd3611256a223273992bc11d56b05447048
logo448 <stenzellogan@gmail.com>
2014-12-13 00:16:43 -0600

    Added binary clock to 'master' from old repo.

---
+++ c#/binaryClock/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace binaryClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.checkBox11 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox12 = new System.Windows.Forms.CheckBox();
+            this.checkBox13 = new System.Windows.Forms.CheckBox();
+            this.checkBox14 = new System.Windows.Forms.CheckBox();
+            this.checkBox15 = new System.Windows.Forms.CheckBox();
+            this.checkBox16 = new System.Windows.Forms.CheckBox();
+            this.checkBox17 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Hours";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Minutes";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(25, 189);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(38, 21);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(25, 162);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(38, 21);
+            this.checkBox2.TabIndex = 13;
+            this.checkBox2.Text = "2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(25, 135);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(38, 21);
+            this.checkBox3.TabIndex = 14;
+            this.checkBox3.Text = "4";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(25, 108);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(38, 21);
+            this.checkBox4.TabIndex = 15;
+            this.checkBox4.Text = "8";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(122, 189);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(38, 21);
+            this.checkBox5.TabIndex = 19;
+            this.checkBox5.Text = "1";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(122, 162);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(38, 21);
+            this.checkBox6.TabIndex = 18;
+            this.checkBox6.Text = "2";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(122, 135);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(38, 21);
+            this.checkBox7.TabIndex = 17;
+            this.checkBox7.Text = "4";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(122, 108);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(38, 21);
+            this.checkBox8.TabIndex = 16;
+            this.checkBox8.Text = "8";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(122, 81);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(46, 21);
+            this.checkBox9.TabIndex = 20;
+            this.checkBox9.Text = "16";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(122, 54);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(46, 21);
+            this.checkBox10.TabIndex = 21;
+            this.checkBox10.Text = "32";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // checkBox11
+            // 
+            this.checkBox11.AutoSize = true;
+            this.checkBox11.Location = new System.Drawing.Point(25, 81);
+            this.checkBox11.Name = "checkBox11";
+            this.checkBox11.Size = new System.Drawing.Size(46, 21);
+            this.checkBox11.TabIndex = 22;
+            this.checkBox11.Text = "16";
+            this.checkBox11.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(197, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 17);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Seconds";
+            // 
+            // checkBox12
+            // 
+            this.checkBox12.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox12.AutoSize = true;
+            this.checkBox12.Location = new System.Drawing.Point(200, 189);
+            this.checkBox12.Name = "checkBox12";
+            this.checkBox12.Size = new System.Drawing.Size(38, 21);
+            this.checkBox12.TabIndex = 29;
+            this.checkBox12.Text = "1";
+            this.checkBox12.UseVisualStyleBackColor = true;
+            // 
+            // checkBox13
+            // 
+            this.checkBox13.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox13.AutoSize = true;
+            this.checkBox13.Location = new System.Drawing.Point(200, 162);
+            this.checkBox13.Name = "checkBox13";
+            this.checkBox13.Size = new System.Drawing.Size(38, 21);
+            this.checkBox13.TabIndex = 28;
+            this.checkBox13.Text = "2";
+            this.checkBox13.UseVisualStyleBackColor = true;
+            // 
+            // checkBox14
+            // 
+            this.checkBox14.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox14.AutoSize = true;
+            this.checkBox14.Location = new System.Drawing.Point(200, 135);
+            this.checkBox14.Name = "checkBox14";
+            this.checkBox14.Size = new System.Drawing.Size(38, 21);
+            this.checkBox14.TabIndex = 27;
+            this.checkBox14.Text = "4";
+            this.checkBox14.UseVisualStyleBackColor = true;
+            // 
+            // checkBox15
+            // 
+            this.checkBox15.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox15.AutoSize = true;
+            this.checkBox15.Location = new System.Drawing.Point(200, 108);
+            this.checkBox15.Name = "checkBox15";
+            this.checkBox15.Size = new System.Drawing.Size(38, 21);
+            this.checkBox15.TabIndex = 26;
+            this.checkBox15.Text = "8";
+            this.checkBox15.UseVisualStyleBackColor = true;
+            // 
+            // checkBox16
+            // 
+            this.checkBox16.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox16.AutoSize = true;
+            this.checkBox16.Location = new System.Drawing.Point(200, 81);
+            this.checkBox16.Name = "checkBox16";
+            this.checkBox16.Size = new System.Drawing.Size(46, 21);
+            this.checkBox16.TabIndex = 25;
+            this.checkBox16.Text = "16";
+            this.checkBox16.UseVisualStyleBackColor = true;
+            // 
+            // checkBox17
+            // 
+            this.checkBox17.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox17.AutoSize = true;
+            this.checkBox17.Location = new System.Drawing.Point(200, 54);
+            this.checkBox17.Name = "checkBox17";
+            this.checkBox17.Size = new System.Drawing.Size(46, 21);
+            this.checkBox17.TabIndex = 24;
+            this.checkBox17.Text = "32";
+            this.checkBox17.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.checkBox12);
+            this.Controls.Add(this.checkBox13);
+            this.Controls.Add(this.checkBox14);
+            this.Controls.Add(this.checkBox15);
+            this.Controls.Add(this.checkBox16);
+            this.Controls.Add(this.checkBox17);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox11);
+            this.Controls.Add(this.checkBox10);
+            this.Controls.Add(this.checkBox9);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Binary Clock";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.CheckBox checkBox11;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox12;
+        private System.Windows.Forms.CheckBox checkBox13;
+        private System.Windows.Forms.CheckBox checkBox14;
+        private System.Windows.Forms.CheckBox checkBox15;
+        private System.Windows.Forms.CheckBox checkBox16;
+        private System.Windows.Forms.CheckBox checkBox17;
+    }
+}
+
---
+++ c#/binaryClock/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+
+namespace binaryClock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// start main thread on load
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // create the main thread
+            Thread main = new Thread(new ThreadStart(Main_Loop));
+            // set thread as background thread so it close automagically on app close
+            main.IsBackground = true;
+            // run the main thread
+            main.Start();
+        }
+
+        /// <summary>
+        /// main function
+        /// </summary>
+        private void Main_Loop()
+        {
+            #region prepare for main loop of function
+            // set the current time to a variable
+            TimeSpan cur_time = DateTime.Now.TimeOfDay;
+
+            // set a sleep var variable to determine how long to sleep in between each iteration
+            // initially set to adjust for the current time
+            int sleep_var = 1000 - cur_time.Milliseconds;       
+
+            // variables to hold current hour, minutes, and seconds
+            int hour = cur_time.Hours;
+            int mins = cur_time.Minutes;
+            int secs = cur_time.Seconds;
+
+            // variables to hold current hour, minutes, and seconds in binary
+            string hr_binary;
+            string min_binary;
+            string sec_binary;
+            #endregion
+
+            // infinite loop
+            while (true)
+            {
+                #region get current time
+                // update current time
+                cur_time = DateTime.Now.TimeOfDay;
+                // get current secs
+                secs = cur_time.Seconds;               
+
+                // check to see if it is a new second
+                if (secs == 0)
+                {
+                    // set the minutes
+                    mins = cur_time.Minutes;
+                }
+
+                // check to see if it is a new hour
+                if (mins == 0)
+                {
+                    // set the hour
+                    hour = cur_time.Hours;
+                }
+                #endregion
+
+                // convert hrs, mins, and seconds to binary
+                hr_binary = Convert.ToString(hour, 2);
+                min_binary = Convert.ToString(mins, 2);
+                sec_binary = Convert.ToString(secs, 2);
+
+                #region append zeros to end of binary numbers if numbers aren't large enough
+                // call append zeros function for hours
+                hr_binary = append_zeros(hr_binary, 5);
+
+                // call append zeros function for minutes 
+                min_binary = append_zeros(min_binary, 6);
+
+                // call append zeros function for seconds
+                sec_binary = append_zeros(sec_binary, 6);
+                #endregion
+
+                #region check and uncheck checkboxes
+
+                #region hours
+                // create tmp array to initilize list
+                CheckBox[] tmp = {checkBox11,
+                    checkBox4,
+                    checkBox3,
+                    checkBox2,
+                    checkBox1};
+                // create list with contents of the tmp array
+                // the list contains checkboxes
+                List<CheckBox> input = new List<CheckBox>(tmp);
+
+                // call checkboxes function on the hr_binary string
+                checkboxes(input, hr_binary);
+                #endregion
+
+                #region mins
+                // create tmp array to initilize a list
+                CheckBox[] tmp1 = {checkBox10,
+                                 checkBox9,
+                                 checkBox8,
+                                 checkBox7,
+                                 checkBox6,
+                                 checkBox5};
+                // create list with the contents of the tmp array
+                // the list holds checkboxes
+                List<CheckBox> input1 = new List<CheckBox>(tmp1);
+
+                // call checkboxes function on min_binary string
+                checkboxes(input1, min_binary);
+                #endregion
+
+                #region seconds
+                // create a tmp array to initilize list with
+                CheckBox[] tmp2 = {checkBox17,
+                                  checkBox16,
+                                  checkBox15,
+                                  checkBox14,
+                                  checkBox13,
+                                  checkBox12};
+                // create list wiht contents of the tmp array
+                // the list holds checkboxes
+                List<CheckBox> input2 = new List<CheckBox>(tmp2);
+
+                // call checkboxes function on sec_binary sting
+                checkboxes(input2, sec_binary);
+                #endregion
+                #endregion
+
+                // adjust for the time it took to do the calculations
+                sleep_var = 1000 - cur_time.Milliseconds;
+
+                // sleep for x seconds
+                Thread.Sleep(sleep_var);
+            }
+        }
+
+        // had to create a form closing event for main thread to autoclose
+        // so I'm leaving this event empty
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// function to append zeros to binary strings if the sting isn't long enough
+        /// two inputs: the binary sting and the desired length
+        /// </summary>
+        /// <param name="bin"></param>
+        /// <param name="len"></param>
+        /// <returns></returns>
+        private string append_zeros(string bin, int len)
+        {
+            // check to see if bin is long enough
+            if (bin.Length < len)
+            {
+                // find how many more zeros are needed
+                int zeros_needed = len - bin.Length;
+
+                // add the appropriate amoun of zeros
+                for (int i = 0; i < zeros_needed; i++)
+                {
+                    bin = "0" + bin;
+                }
+                
+                // return binary string
+                return bin;
+            }
+
+            // if bin is already long enough return bin
+            else
+            {
+                // return binary string
+                return bin;
+            }
+        }
+
+        /// <summary>
+        /// a function to check and uncheck checkboxes
+        /// this function is recursive
+        /// </summary>
+        /// <param name="boxes"></param>
+        private Tuple<string, List<CheckBox>> checkboxes(List<CheckBox> boxes, string bin_str)
+        {
+            // get current binary digit
+            string dig = bin_str[0].ToString();
+
+            // base case
+            if (bin_str.Length == 1)
+            {
+                // check/uncheck the proper checkbox
+                // check if checkbox should be checked
+                if (dig == "1")
+                {
+                    // create an action for the invoke method
+                    Action action = () => boxes[0].Checked = true;
+                    // use invoke to access gui element from different thread
+                    this.Invoke(action);
+                }
+                // check if checkbox should be unchecked
+                if (dig == "0")
+                {
+                    // create an action for the invoke method
+                    Action action = () => boxes[0].Checked = false;
+                    // use invoke to access gui element from different thread
+                    this.Invoke(action);
+                }
+
+                // return nothing
+                return null;
+            }
+
+            // case
+            else
+            {
+                // check/uncheck the proper checkbox
+                // check if checkbox should be checked
+                if (dig == "1")
+                {
+                    // create an action for the invoke method
+                    Action action = () => boxes[0].Checked = true;
+                    // use invoke to access gui element from different thread
+                    this.Invoke(action);
+                }
+                // check if checkbox should be unchecked
+                if (dig == "0")
+                {
+                    // create an action for the invoke method
+                    Action action = () => boxes[0].Checked = false;
+                    // use invoke to access gui element from different thread
+                    this.Invoke(action);
+                }
+
+                // decrease length of the binary string and checkbox array
+                var output = Tuple.Create(bin_str.Substring(1), boxes.GetRange(1,(boxes.Capacity - 1)));
+
+                // call function again with shorter binary string and checkbox array
+                return checkboxes(output.Item2, output.Item1);
+            }
+        }
+    }
+}
