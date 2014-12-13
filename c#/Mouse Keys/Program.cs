e1af7b9629e590f5656ec2717d55d9252990a62a
logo448 <stenzellogan@gmail.com>
2014-12-13 16:45:39 -0600

    Added a c# program to dev. The program leverages the console hot key class. The program is my own version of mouse keys.

---
+++ c#/Mouse Keys/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using ConsoleHotKey;
+
+namespace Mouse_Keys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // register my hotkeys
+            HotKeyManager.RegisterHotKey(Keys.A, KeyModifiers.Alt | KeyModifiers.Shift);
+            HotKeyManager.RegisterHotKey(Keys.D, KeyModifiers.Alt | KeyModifiers.Shift);
+            HotKeyManager.RegisterHotKey(Keys.W, KeyModifiers.Alt | KeyModifiers.Shift);
+            HotKeyManager.RegisterHotKey(Keys.S, KeyModifiers.Alt | KeyModifiers.Shift);
+            HotKeyManager.HotKeyPressed += new EventHandler<HotKeyEventArgs>(HotKeyManager_HotKeyPressed);
+
+            // keep program running
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// function that executes when one of the hotkeys is triggered
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static void HotKeyManager_HotKeyPressed(object sender, HotKeyEventArgs e)
+        {
+            if (e.Key == Keys.W)
+            {
+                Console.WriteLine("W");
+            }
+        }
+    }
+}
